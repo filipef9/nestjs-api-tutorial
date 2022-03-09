f3857e5e4260e0309169e83e1f9923dce2f57153
filipef9 <filipef9@hotmail.com>
2022-03-09 15:10:43 +0000

    Implementação do carregamento das configurações .env do banco de dados.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
 
-  constructor() {
+  constructor(private config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:nest@localhost:5432/nest?schema=public'
+          url: config.get('DATABASE_URL')
         }
       }
     })
